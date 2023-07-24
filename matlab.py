75c66bd17b7ba81bf4ea015482691cae2b41fa61
Datta Magar <dattamagar211@gmail.com>
2023-07-24 13:32:29 +0000

    Loaded matlab with imports

---
+++ matlab.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Use seaborn's set_style() function to set the style
+sns.set_style('whitegrid')  # Replace 'whitegrid' with the desired style if needed
+
+# Your code for creating plots goes here
